2a02fc5d981ce340e0b193aaf37b631c1dd4e9e5
Umair <umair@tezrosolutions.com>
2015-10-12 15:45:07 +0500

    Details fixes.

--- config/hubspot.php
+++ config/hubspot.php
@@ -1,26 +1,34 @@
 <?php
+
 $hubspotConfig = array(
     "HUBSPOT_API_KEY" => "6af915fd-806f-483a-b10b-bcb9f94b239d",
     "HUBSPOT_PORTAL_ID" => "695602"
 );
 
 $geniusDealStatuses = array(
+    '0001' => 'genius_in_progress',
     '0003' => 'genius_new_enquiry',
     '0007' => 'genius_declined',
     '0008' => 'genius_withdrawn',
     '0009' => 'genius_approved',
-    '0001' => 'genius_in_progress',
     '0012' => 'genius_application',
-    '0013' => 'genius_lost_no_contact',
+    '0013' => 'genius_no_contact', //U
     '0014' => 'genius_waiting_info',
     '0015' => 'genius_submitted',
     '0016' => 'genius_appointed',
     '0017' => 'genius_app_taken',
     '0018' => 'genius_lost',
     '0019' => 'genius_call_back',
+    '0025' => 'genius_settled_irregular', //N
     '0026' => 'genius_settled',
     '0027' => 'genius_new_sms',
-    '0030' => 'genius_activated'
+    '0028' => 'genius_in_progress_settled', //N
+    '0029' => 'genius_withdrawn_settled', //N
+    '0030' => 'genius_activated',
+    '0031' => 'genius_no_contact_settled', //N
+    '0034' => 'genius_lost_no_contact', //N
+    '0032' => 'genius_waiting_info_settled', //N
+    '0033' => 'genius_call_back_settled', //N
 );
 
 
